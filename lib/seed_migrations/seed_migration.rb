b657e306f4c576e4be3dd86fc32fe77c63772e3d
Matthias Grosser <mtgrosser@gmx.net>
2019-08-19 12:48:21 +0200

    Support Rails 6

--- lib/seed_migrations/seed_migration.rb
+++ lib/seed_migrations/seed_migration.rb
@@ -1,60 +1,64 @@
 class ActiveRecord::SeedMigration < ActiveRecord::Base
   
-  def self.load_seeds
-    puts 'Loading all seeds'
-    ensure_table_is_set
-    Dir.chdir(File.join(Rails.root, "db", "seed"))
-    seeds = Dir.glob('*.rb').sort.each do |file_name|
-      if seed_name = file_name.match(/[0-9]+_(.*).rb/).captures.first
-        unless find_by_name(file_name.chomp('.rb'))
-          ActiveRecord::Base.transaction do                        
-            puts "- Loading data from #{file_name}"    
-            load File.join(Rails.root, "db", "seed", file_name)
-            seed_class = begin
-              seed_name.camelize.constantize
-            rescue NameError => n
-              puts 'No appropriate class specified in the file! Skipping it!'
+  class << self
+
+    def load_seeds
+      puts 'Loading all seeds'
+      ensure_table_is_set
+      Dir.chdir(File.join(Rails.root, "db", "seed"))
+      Dir.glob('*.rb').sort.each do |file_name|
+        if seed_name = file_name.match(/[0-9]+_(.*).rb/).captures.first
+          unless find_by_name(file_name.chomp('.rb'))
+            ActiveRecord::Base.transaction do
+              puts "- Loading data from #{file_name}"
+              load File.join(Rails.root, "db", "seed", file_name)
+              seed_class = begin
+                seed_name.camelize.constantize
+              rescue NameError
+                puts 'No appropriate class specified in the file! Skipping it!'
+              end
+              seed_class.up if seed_class
+              seed = new
+              seed.name = file_name.chomp('.rb')
+              seed.save!
             end
-            seed_class.up if seed_class
-            seed = new
-            seed.name = file_name.chomp('.rb')
-            seed.save!
           end
         end
       end
     end
-  end
   
-  def self.reload_last_seed
-    puts 'Reloading last seed'
-    ensure_table_is_set
-    if last_seeding = last
-      seed_name = last_seeding.name.match(/[0-9]+_(.*)/).captures.first
-      ActiveRecord::Base.transaction do        
-        puts "- Loading data from #{last_seeding.name}.rb"
-        load File.join(Rails.root, "db", "seed", last_seeding.name + '.rb')
-        seed_class = begin
-          seed_name.camelize.constantize
-        rescue NameError => n
-          puts 'No appropriate class specified in the file! Skipping it!'
+    def reload_last_seed
+      puts 'Reloading last seed'
+      ensure_table_is_set
+      if last_seeding = last
+        seed_name = last_seeding.name.match(/[0-9]+_(.*)/).captures.first
+        ActiveRecord::Base.transaction do
+          puts "- Loading data from #{last_seeding.name}.rb"
+          load File.join(Rails.root, "db", "seed", last_seeding.name + '.rb')
+          seed_class = begin
+            seed_name.camelize.constantize
+          rescue NameError
+            puts 'No appropriate class specified in the file! Skipping it!'
+          end
+          seed_class.up if seed_class
         end
-        seed_class.up if seed_class
       end
     end
-  end
   
-  private 
+    private
   
-  def self.ensure_table_is_set
-    create_seed_migrations_table unless ActiveRecord::Migration.data_source_exists?("seed_migrations")
-  end
-  
-  def self.create_seed_migrations_table
-    ActiveRecord::Migration.create_table :seed_migrations do |t|
-      t.string :name, :null => false
-      t.timestamps
+    def ensure_table_is_set
+      create_seed_migrations_table unless ActiveRecord::Migration.data_source_exists?("seed_migrations")
     end
-    ActiveRecord::Migration.add_index("seed_migrations", :name, :unique => true)
+  
+    def create_seed_migrations_table
+      ActiveRecord::Migration.create_table :seed_migrations do |t|
+        t.string :name, :null => false
+        t.timestamps
+      end
+      ActiveRecord::Migration.add_index("seed_migrations", :name, :unique => true)
+    end
+
   end
 
 end
