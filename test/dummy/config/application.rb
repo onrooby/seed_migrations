99cd5c302cfc4b41838bae51285a53ce18f2a4b5
Matthias Grosser <mtgrosser@gmx.net>
2022-03-28 15:13:05 +0200

    Support Rails 7

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -39,12 +39,8 @@ module Dummy
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    
+    config.active_record.legacy_connection_handling = false
   end
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,6 +20,7 @@ ENV["RAILS_ENV"] = "test"
 
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
+require 'rake'
 
 Rails.backtrace_cleaner.remove_silencers!
 
